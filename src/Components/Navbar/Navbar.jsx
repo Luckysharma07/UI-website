5407105cf4dd07649031b3ffe0c52d263dd51fb5
Lucky0742 <luckysharma@yojnakarinnovation.com>
2026-05-12 19:03:06 +0530

    Fixed asset path case issue

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
                                                 
   return (
     <nav className={`container ${sticky? 'dark-nav' :  ''}`}>
-            <img src={logo} alt="" className='logo'/>
+            <img src={logo} alt="Edusity Logo" className='logo'/>
             <ul className={mobileMenu?'':'hide-mobile-menu'}>
                 <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
                 <li><Link to='Programs' smooth={true} offset={-260} duration={500}>Programs</Link></li>
